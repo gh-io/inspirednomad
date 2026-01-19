536da13754123bae2266ffc077289d1bfe13efb1
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-19 14:53:29 +0100

    Create Send.php

---
+++ Backend/API/Send.php
@@ -0,0 +1,24 @@
+<?php   
+ 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://localhost:8080/api/send.php',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => 'api_key=YOUR-SECRET-API-KEY&mobile=923XXX&message=HelloWorld',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/x-www-form-urlencoded'
+  ),
+));
+ 
+$response = curl_exec($curl);
+ 
+curl_close($curl);
+echo $response;
+ 
+?>
