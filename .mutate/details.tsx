9a19385a2e715de7fd1b2d3c676de402cdce5443
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-19 14:21:52 +0100

    Create details.tsx

---
+++ .mutate/details.tsx
@@ -0,0 +1,16 @@
+import './Detail.css';
+import { trpc } from '../App';
+function Detail(props: {
+  id: number,
+}) {
+  const cat = trpc.get.useQuery(props.id);
+  return (
+    cat.data ? 
+      <div className="Detail">
+        <h2>Detail</h2>
+        <div>{cat.data.id}</div>
+        <div>{cat.data.name}</div>
+      </div> : <div className="Detail"></div>
+  );
+}
+export default Detail;
